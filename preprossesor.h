e11423e67048ed5d2b15236b2c4e82fddcfd80b2
YANB121 <107056123+YANB121@users.noreply.github.com>
2022-07-08 17:40:38 +0300

    Update preprossesor.h

--- preprossesor.h
+++ preprossesor.h
@@ -1,1 +1,17 @@
 
+#ifndef MAMN14_PREPROCESSOR_H
+#define MAMN14_PREPROCESSOR_H
+
+#include "hashmap.h"
+
+FILE *create_macro_file(char *);
+
+void span_macros(char *);
+
+void fetch_write_macros(FILE *file, HashTable *hashTable, char *);
+
+void write_macros(FILE *, HashTable);
+
+void macro_file_path(char *);
+
+#endif //MAMN14_PREPROCESSOR_H
